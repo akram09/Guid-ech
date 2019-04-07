14c723f3792f2642cae95cb6656b87c0cd2443cc
Moncef <48018221+MoncefReg@users.noreply.github.com>
2019-04-07 19:02:33 +0100

    Add files via upload

---
+++ database/migrations/2019_04_07_162424_add_column_sliders_restau_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnSlidersRestauId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sliders', function (Blueprint $table) {
+            $table->integer('restau')->unsigned();
+            $table->foreign('restau')->references('id')->on('restaurant');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sliders', function (Blueprint $table) {
+            $table->dropForeign(['restau']);
+            $table->dropColumn('restaur');
+        });
+    }
+}
