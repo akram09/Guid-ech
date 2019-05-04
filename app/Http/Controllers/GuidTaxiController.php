f1d3c943fa879a49776634acfe84ff81fdcf5966
Dermi Malika <48280218+Malika3@users.noreply.github.com>
2019-05-03 13:44:18 -1100

    Create GuidTaxiController.php

---
+++ app/Http/Controllers/GuidTaxiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+class GuidTaxiController extends Controller
+{
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function afficher($wilayas_id){
+    	$elements = DB::table('GuidTaxi')->where('wilaya_id', strval($wilayas_id))->paginate(5);
+
+        return view('GuidTaxi', compact('elements'));
+
+    }
+    public function affichertt(){
+
+        $elements = DB::table('GuidTaxi')->get();
+
+        return view('GuidTaxi' , compact('elements'));
+    }
+
+    }
+
+   /*
+    public function index()
+    {
+        return view('GuidTaxi');
+    }*/
+
