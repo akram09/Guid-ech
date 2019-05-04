703baed3712c06eed5fb6bf97ceb3fe2306b6a8f
Dermi Malika <48280218+Malika3@users.noreply.github.com>
2019-05-03 13:43:19 -1100

    Create GuidTaxi.blade.php

---
+++ resources/views/GuidTaxi.blade.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<title>GuidTaxi</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('affichageDisplay/css/bootstrap.min.css') }}" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="{{ asset('affichageDisplay/css/mdb.min.css') }}" rel="stylesheet">
+  <!-- our custom styles (optional) ila bghitou tmodifiw -->
+  <link href="{{ asset('affichageDisplay/css/style.min.css') }}" rel="stylesheet">
+  <!-- CSRF Token -->
+ <meta name="csrf-token" content="{{ csrf_token() }}">
+ 
+  <style type="text/css">
+
+html,
+    body,
+    header,
+    .view {
+      height: 100%;
+    }
+
+    @media (max-width: 740px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 1000px;
+      }
+    }
+
+    @media (min-width: 800px) and (max-width: 850px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 650px;
+      }
+    }
+    @media (min-width: 800px) and (max-width: 850px) {
+              .navbar:not(.top-nav-collapse) {
+                  background:#1C2331!important;
+              }
+          }
+
+
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column (if you want) */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 10px;
+}
+
+/* The "show" class is added to the filtered elements */
+.show {
+  display: block;
+}
+
+/* Style the buttons 
+.btn {
+  border: none;
+  outline: none;
+  padding: 12px 16px;
+  background-color: white;
+  cursor: pointer;
+}*/
+
+/* Add a grey background color on mouse-over 
+.btn:hover {
+  background-color: #ddd;
+}
+*/
+/* Add a dark background color to the active button 
+.btn.active {
+  background-color: #666;
+   color: white;
+}*/
+
+  </style>
+
+</head>
+
+<body >
+   <!-- Navbar  -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="#" target="_blank">
+        <strong>Guidech</strong>
+        
+      </a>
+
+      
+&nbsp; &nbsp;
+&nbsp;
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ url('/home') }}">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!--
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">bla bla</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">bla bla 2</a>
+          </li>
+        -->
+
+        </ul>
+        <!-- Collapsible content -->
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <form class="form-inline ml-auto">
+                 <div class="md-form my-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                </div>
+                   <button class="btn btn-outline-primary btn-rounded waves-effect btn-sm" type="submit"><i class="fas fa-search"></i> OK</button>
+               </form>
+
+  </div>
+  <!-- Collapsible content -->
+
+         
+
+        <!-- Right -->
+        <ul class="navbar-nav nav-flex-icons"> <!-- affichage username et la photo -->
+         
+
+                  <div class="dropdown">
+             <a href="#" class="btn btn-info dropdown-toggle" data-toggle="dropdown" id="dropdownMenu2" aria-haspopup="true" role="button" aria-expanded="false" style="position:relative; padding-left:50px;">
+                  <img src="/uploads/avatars/{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:absolute; top:10px; left:10px; border-radius:50%"> {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+
+<div class="dropdown-menu dropdown-primary">  <!-- drop menu logout and change password -->
+  @csrf
+  <a class="dropdown-item" href="{{ url('/changePassword') }}"><i class="fas fa-user-edit"></i>change password</a>
+  <div >
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+
+             <button type="button" class="btn btn-danger">Logout</button>
+
+        </a>
+
+     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+    </form>
+</div>
+    
+ </div>
+        </div>
+
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+<!-- Full Page Intro -->
+<div class="view full-page-intro" style="background-image: url('/images/taxi.jpg') ; background-repeat: no-repeat; background-size: cover;">
+</div>
+<!----------------------->
+<h1 class="my-5 h1 text-center">Voici quelques numéros des taxieurs disponible </h1>
+<!-- Portfolio Gallery Grid -->
+<div class="row">
+   @foreach($elements as $element)
+  <div class="column ">
+  
+    <div class="content">
+      <img src="/images/icone.png" alt="Taxi_Icone" style="width:70%">
+      <h4>{{ $element->NumTel }} </h4> <!--numéro du taxieur-->
+      <p>{{ $element->Details }}</p>
+    </div>
+    
+  </div>
+  @endforeach
+</div>
+     
+
+<!--Footer-->
+  <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+ 
+
+    <hr class="my-4">
+
+    <!-- Social icons -->
+    <div class="pb-4">
+      <a href="#" target="_blank">
+        <i class="fab fa-facebook-f mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-google-plus-g mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-dribbble mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-pinterest mr-3"></i>
+      </a>
+
+      <a href="#" target="_blank">
+        <i class="fab fa-github mr-3"></i>
+      </a>
+
+      <a href="#/" target="_blank">
+        <i class="fab fa-codepen mr-3"></i>
+      </a>
+    </div>
+    <!-- Social icons -->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2019 Copyright:
+      <a href="#" target="_blank"> Esi-sba </a>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
+
+  <!-- SCRIPTS -->
+    <!-- JQuery -->
+  <script type="text/javascript" src="{{ asset('affichageDisplay/js/jquery-3.3.1.min.js') }}"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="{{ asset('affichageDisplay/js/popper.min.js') }}"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="{{ asset('affichageDisplay/js/bootstrap.min.js') }}"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+  </script>
+
+</body>
+</html>
