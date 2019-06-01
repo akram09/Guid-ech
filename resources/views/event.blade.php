896b33607cdbd19d122cbde5290ea54dd9e72b15
wahidmdn13 <wahidmadano@gmail.com>
2019-06-01 17:44:58 +0200

    adding event calendar

---
+++ resources/views/event.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">  
+  <!-- CSRF Token -->
+ <meta name="csrf-token" content="{{ csrf_token() }}">
+
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+</head>
+
+<body>
+<div class="container">
+@if (\Session::has('success'))
+      <div class="alert alert-success">
+        <p>{{ \Session::get('success') }}</p>
+      </div><br />
+     @endif
+ <div class="panel panel-primary">
+              <div class="panel-heading">Guidech events</div>
+              <div class="panel-body" >
+                  {!! $calendar_details->calendar() !!}
+              </div>
+            </div>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+{!! $calendar_details->script() !!}
+</body>
+</html>
+
